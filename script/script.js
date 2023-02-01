23ccae405d09a68525c6da9c37a83cad38ca0419
elcorre <correa755@hotmail.com>
2023-01-31 22:34:43 -0300

    Implementando botones, quitar la seleccionar al presionar un boton

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ let btn = document.getElementsByClassName("btn")
 let btn_array = Array.from(btn)
 
 
-console.log("<-----Botones----->" , "\n" , ...btn_array)
+//////NUMEROS//////
 
 btn_array.forEach(boton => {
     boton.addEventListener("click" , ()=>{
@@ -28,28 +28,25 @@ btn_array.forEach(boton => {
 })
 
 
+//////OPERADORES//////
+
 let btn_operador = document.getElementsByClassName("btn__operador")
 
 let btn_array_operador = Array.from(btn_operador)
 
-//////INTRODUCIR//////
-
 btn_array_operador.forEach(boton => {
     boton.addEventListener("click" , ()=>{
-        sonido_btn_comun.play()
-        sonido_btn_comun.currentTime = 0
         btn_operador = boton.value
 
-        if(display.value[display.value.length-1] == btn_operador){
+        if(display.value[display.value.length-1] == btn_operador || display.value == ""){ //SI EL ÚLTIMO CARACTER DEL DISPLAY ES UN OPERADOR, NO AGREGA EL OPERADOR.
 
         }
-
         else{
-            display.value += btn_operador
+            sonido_btn_comun.play()
+            sonido_btn_comun.currentTime = 0
+            display.value += btn_operador //SINO SI
         }
 
-        
-        
     })
 })
 
@@ -58,33 +55,91 @@ btn_array_operador.forEach(boton => {
 
 let btn_clear = document.getElementById("btn_clear")
 
+let form = document.getElementById("calculadora")
+
 btn_clear.addEventListener("click" , ()=>{
-    sonido_clear.play()
-    sonido_clear.currentTime = 0
-    display.value = "";
+    clear();
 })
+
+function clear(){
+    if(display.value != ""){
+        sonido_clear.play()
+        sonido_clear.currentTime = 0
+    }
+
+    form.reset()
+}
 
 
 //////IGUAL///////
+
 let resultado = []
 let btn_resultado = document.getElementById("btn_resultado")
+let visor = document.getElementById("display")
 
 btn_resultado.addEventListener("click" , ()=>{
-    let visor = document.getElementById("display")
-    sonido_resultado.play()
-    sonido_resultado.currentTime = 0
-    display.value = eval(display.value)
-
-
-    visor.value = parseFloat(display.value)
-
-    if(isNaN(visor.value)){
-        visor.value = "";
-    }
-
-    resultado.push(visor.value)
-    
-    console.log("RESULTADO:" , visor.value)
+    resolver();
 })
 
+function resolver(){
+    if(display.value != ""){
+        sonido_resultado.play()
+        sonido_resultado.currentTime = 0
+        display.value = eval(display.value)
+    
+        visor.value = parseFloat(display.value)
+    
+        if(isNaN(visor.value)){
+            visor.value = "";
+        }
+    
+        resultado.push(visor.value)
+        
+        console.log("RESULTADO:" , visor.value)
+    }
+}
+
+/////// TECLADO ///////
+
+document.addEventListener("keypress" , (e) =>{
+    let tecla = e.key
+    
+    if(tecla == 0 || tecla == 1 || tecla == 2 || tecla == 3 || tecla == 4 || tecla == 5 || tecla == 6 || tecla == 7 || tecla == 8 || tecla == 9){
+        sonido_btn_comun.play()
+        sonido_btn_comun.currentTime = 0
+        tecla = parseInt(tecla)
+        display.value += tecla
+    }
+
+    else if(tecla == "/" || tecla == "*"  || tecla == "-" || tecla == "+"){
+
+        if(display.value[display.value.length-1] == tecla || display.value == ""){ //SI EL ÚLTIMO CARACTER DEL DISPLAY ES UN OPERADOR, NO AGREGA EL OPERADOR.
+        }
+        else{
+            sonido_btn_comun.play()
+            sonido_btn_comun.currentTime = 0
+            display.value += tecla //SINO SI
+        }
+    }
+
+    else if(tecla == "Enter" && display.value != ""){
+
+            sonido_resultado.play()
+            sonido_resultado.currentTime = 0
+    
+            display.value = eval(display.value)
+            visor.value = parseFloat(display.value)
+    
+            if(isNaN(visor.value)){
+                visor.value = "";
+            }
+    
+            resultado.push(visor.value)
+            console.log(display.value)
+            console.log("RESULTADO:" , visor.value)
+
+        
+    }
+
+})
 
